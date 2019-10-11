132fa54a961107b30e810978d5c50838d5738bde
Mattia Gheda <ghedamat@gmail.com>
2019-10-11 09:56:31 -0400

    emer packages

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 self: super:
 
+with super;
 {
   shellStuff = super.stdenv.mkDerivation rec {
     emberShellHooks = ''
@@ -8,5 +9,24 @@ self: super:
       export NPM_CONFIG_PREFIX=$PWD/.nix-node
       export PATH=$NODE_PATH/bin:$PATH
     '';
+
+    basePackages = [
+      python
+      nodejs-10_x
+      pkgs.yarn
+      gnumake
+      gcc
+      readline
+      openssl
+      zlib
+      libxml2
+      curl
+      libiconv
+    ];
+
+    emberPkgs = if system == "x86_64-darwin" then
+      basePackages ++ [darwin.apple_sdk.frameworks.CoreServices]
+    else
+      basePackages;
   };
 }
