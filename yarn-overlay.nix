5842b738316b07f30112bc75fb520d66b65183b3
Mattia Gheda <ghedamat@gmail.com>
2019-10-10 21:19:09 -0400

    sadf

--- yarn-overlay.nix
+++ yarn-overlay.nix
@@ -5,7 +5,7 @@ self: super:
     pname = "yarn";
     version = "1.19.0";
 
-    src = builtins.fetchzip {
+    src = super.fetchzip {
       url = "https://github.com/yarnpkg/yarn/releases/download/v${version}/yarn-v${version}.tar.gz";
       sha256 = "14h293kixxiww41daj0xyjii8d9mgr2ah5sxadass60p61ynxsq4";
     };
