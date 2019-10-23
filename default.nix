1e2082d2f056cfae47293e5adf5c3825e366e90a
Mattia Gheda <ghedamat@gmail.com>
2019-10-23 00:35:31 +0000

    destroy!

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 # Nixpkgs overlay which aggregates overlays for tools and products, used and
 # published by PN.
-self: super: rec {
+self: super:
+  let
 	yarn = super.yarn.overrideAttrs (oldAttrs: rec {
 		version = "1.19.0";
 
@@ -10,13 +11,6 @@ self: super: rec {
 		};
 		buildInputs = [ super.nodejs-10_x ];
 	});
-
-  emberShellHooks = ''
-      mkdir -p .nix-node
-      export NODE_PATH=$PWD/.nix-node
-      export NPM_CONFIG_PREFIX=$PWD/.nix-node
-      export PATH=$NODE_PATH/bin:$PATH
-  '';
 
   basePackages = [
     yarn
@@ -30,6 +24,15 @@ self: super: rec {
     super.curl
     super.libiconv
   ];
+  in
+  {
+
+  emberShellHooks = ''
+      mkdir -p .nix-node
+      export NODE_PATH=$PWD/.nix-node
+      export NPM_CONFIG_PREFIX=$PWD/.nix-node
+      export PATH=$NODE_PATH/bin:$PATH
+  '';
 
   emberPkgs = if super.system == "x86_64-darwin" then
     basePackages ++ [super.darwin.apple_sdk.frameworks.CoreServices]
