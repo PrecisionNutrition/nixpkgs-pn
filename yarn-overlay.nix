6c76d53ff4ecab2aaca23cafb0bfc25419cb931d
Mattia Gheda <ghedamat@gmail.com>
2019-10-23 00:21:14 +0000

    different yarn

--- yarn-overlay.nix
+++ yarn-overlay.nix
@@ -1,30 +1,13 @@
 self: super:
 
 {
-  yarn = super.stdenv.mkDerivation rec {
-    pname = "yarn";
-    version = "1.19.0";
+	yarn = super.yarn.overrideAttrs (oldAttrs: rec {
+		version = "1.19.0";
 
-    src = super.fetchzip {
-      url = "https://github.com/yarnpkg/yarn/releases/download/v${version}/yarn-v${version}.tar.gz";
-      sha256 = "14h293kixxiww41daj0xyjii8d9mgr2ah5sxadass60p61ynxsq4";
-    };
-
-    buildInputs = [ super.nodejs-10_x ];
-
-    installPhase = ''
-        mkdir -p $out/{bin,libexec/yarn/}
-        cp -R . $out/libexec/yarn
-        ln -s $out/libexec/yarn/bin/yarn.js $out/bin/yarn
-        ln -s $out/libexec/yarn/bin/yarn.js $out/bin/yarnpkg
-    '';
-
-    meta = with super.stdenv.lib; {
-      homepage = https://yarnpkg.com/;
-      description = "Fast, reliable, and secure dependency management for javascript";
-      license = licenses.bsd2;
-      maintainers = with maintainers; [ offline screendriver ];
-      platforms = platforms.linux ++ platforms.darwin;
-    };
-  };
+		src = super.fetchzip {
+			url = "https://github.com/yarnpkg/yarn/releases/download/v${version}/yarn-v${version}.tar.gz";
+			sha256 = "14h293kixxiww41daj0xyjii8d9mgr2ah5sxadass60p61ynxsq4";
+		};
+		buildInputs = [ super.nodejs-10_x ];
+	});
 }
