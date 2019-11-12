8c7e9f8046f1a0baa80d0f790184a64f2a7bd7e4
Mattia Gheda <ghedamat@gmail.com>
2019-11-12 01:23:09 +0000

    test

--- yarn-overlay.nix
+++ yarn-overlay.nix
@@ -1,14 +1,16 @@
 self: super:
 
-{
-
-  yarn = super.yarn.overrideAttrs (oldAttrs: rec {
-    version = "1.19.0";
+let
     pkgs = import (builtins.fetchTarball {
       name = "nixos-19.03";  # Descriptive name
       url = https://github.com/nixos/nixpkgs-channels/archive/aea8dde8b321e373c71eb4e785c1cacd2824c878.tar.gz;
       sha256 = "092sw0igmnbxg3azkdijhydxl4r9gb1z0593c4nx1d09y3fb5gcb";
     }) {};
+in
+{
+
+  yarn = pkgs.yarn.overrideAttrs (oldAttrs: rec {
+    version = "1.19.0";
 
     src = super.fetchzip {
       url = "https://github.com/yarnpkg/yarn/releases/download/v${version}/yarn-v${version}.tar.gz";
