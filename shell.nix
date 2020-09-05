9e5d353fc147659ce1b885032c24f21089a99144
Mattia Gheda <ghedamat@gmail.com>
2020-09-05 13:39:56 +0000

    typo

--- shell.nix
+++ shell.nix
@@ -2,8 +2,8 @@ self: super:
 
 let
     pkgs = import (builtins.fetchTarball {
-      name = "nixos-19.09-2020-02-05";  # Descriptive name
-      url = https://github.com/nixos/nixpkgs-channels/archive/ea553d8c67c6a718448da50826ff5b6916dc9c59.tar.gz;
+      name = "nixos-20.03-2020-09-05";  # Descriptive name
+      url = https://github.com/nixos/nixpkgs-channels/archive/51d115ac89d676345b05a0694b23bd2691bf708a.tar.gz;
       sha256 = "0g9smv36sk42rfyzi8wyq2wl11c5l0qaldij1zjdj60s57cl3wgj";
     }) {};
 in
--- yarn-overlay.nix
+++ yarn-overlay.nix
@@ -3,7 +3,7 @@ self: super:
 let
     pkgs = import (builtins.fetchTarball {
       name = "nixos-20.03-2020-09-05";  # Descriptive name
-      url = https://github.com/nixos/nixpkgs-channels/archive/51d115ac89d676345b05a0694b23bd2691bf708a;
+      url = https://github.com/nixos/nixpkgs-channels/archive/51d115ac89d676345b05a0694b23bd2691bf708a.tar.gz;
       sha256 = "0g9smv36sk42rfyzi8wyq2wl11c5l0qaldij1zjdj60s57cl3wgj";
     }) {};
 in
