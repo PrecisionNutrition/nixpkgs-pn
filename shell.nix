624531a14f4f9bd03872cd77b380f471220d9c3d
Mattia Gheda <ghedamat@gmail.com>
2021-02-05 15:11:30 +0000

    add niv

--- shell.nix
+++ shell.nix
@@ -1,11 +1,7 @@
 self: super:
 
 let
-    pkgs = import (builtins.fetchTarball {
-      name = "nixos-unstable-2020-12-20";  # Descriptive name
-      url = "https://github.com/NixOS/nixpkgs/archive/e9158eca70ae59e73fae23be5d13d3fa0cfc78b4.tar.gz";
-      sha256 = "0cnmvnvin9ixzl98fmlm3g17l6w95gifqfb3rfxs55c0wj2ddy53";
-    }) {};
+    pkgs = import ( ./nix/sources.nix ) {};
 in
 {
   shellStuff = super.stdenv.mkDerivation rec {
