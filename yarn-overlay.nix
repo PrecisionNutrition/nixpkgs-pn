0e1e2fb78ce00f42ac9303c4a9dac2d8a7e7e088
Mattia Gheda <ghedamat@gmail.com>
2021-02-05 15:22:18 +0000

    foo

--- yarn-overlay.nix
+++ yarn-overlay.nix
@@ -1,11 +1,9 @@
+# currently unused as nixpkgs 20.09 has a version we like
 self: super:
 
 let
-    pkgs = import (builtins.fetchTarball {
-      name = "nixos-unstable-2020-12-20";  # Descriptive name
-      url = "https://github.com/NixOS/nixpkgs/archive/e9158eca70ae59e73fae23be5d13d3fa0cfc78b4.tar.gz";
-      sha256 = "0cnmvnvin9ixzl98fmlm3g17l6w95gifqfb3rfxs55c0wj2ddy53";
-    }) {};
+    sources = import ( ./nix/sources.nix ) {};
+    pkgs = import sources.nixpkgs {};
 in
 {
 
